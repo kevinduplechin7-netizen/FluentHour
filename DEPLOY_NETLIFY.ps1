7378c04246cf02e7197b7d8a4a42b73f5fbba908
kevinduplechin7-netizen <kevinduplechin7@gmail.com>
2026-01-06 09:15:11 -0600

    Add one-command Netlify deploy script

---
+++ DEPLOY_NETLIFY.ps1
@@ -0,0 +1,14 @@
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference="Stop"
+
+cd $PSScriptRoot
+
+git checkout main
+git pull origin main
+
+npm ci
+npm run build
+
+netlify link --id e35b36f5-9bd8-446a-ad1d-48f11aa14f0f
+$sha = (git rev-parse --short HEAD)
+netlify deploy --prod --dir=dist --message "deploy $sha main"
